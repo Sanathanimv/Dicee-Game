20bcd544c7502e5767c69b709e60010a3547cf7c
Srujanasgr <srujanamv123@gmail.com>
2026-05-13 10:25:07 +0530

    changed innerHTML content

--- index.js
+++ index.js
@@ -18,11 +18,11 @@ document.querySelector(".img2").setAttribute("src", image2);
 
 //winner
 if(randomNumber1>randomNumber2){
-    document.querySelector("h1").innerHTML="Player 1 Won";
+    document.querySelector("h1").innerHTML="Player 1 Wins!";
 }
 else if(randomNumber2>randomNumber1){
-    document.querySelector("h1").innerHTML="Player 2 Won";
+    document.querySelector("h1").innerHTML="Player 2 Wins!";
 }
 else{
-document.querySelector("h1").innerHTML="Draw";
+document.querySelector("h1").innerHTML="Draw!!!";
 }
